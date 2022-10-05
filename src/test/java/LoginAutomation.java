e15b603fed9079d8231da071570931f84fed36b3
Vlad <vshylepnytskyi@9bits.pl>
2022-10-05 15:37:13 +0200

    new tests

--- src/test/java/LoginAutomation.java
+++ src/test/java/LoginAutomation.java
@@ -2,26 +2,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class LoginAutomation {
     @Test
-    public void login() {
+    public void login() throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "/Users/vlad/Documents/Selenium-drivers/Firefox/geckodriver");
         WebDriver driver=new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.browserstack.com/users/sign_in");
-        WebElement username=driver.findElement(By.id("user_email_login"));
-        WebElement password=driver.findElement(By.id("user_password"));
-        WebElement login=driver.findElement(By.name("commit"));
-        username.sendKeys("abc@gmail.com");
-        password.sendKeys("your_password");
-        login.click();
-        WebElement error=driver.findElement(By.className("error-msg"));
-        String actualUrl="https://live.browserstack.com/dashboard";
-        String expectedUrl= driver.getCurrentUrl();
-        Assert.assertNotNull(error);
+        driver.get("https://automationpractice.com/index.php");
+        WebElement product=driver.findElement(By.id("search_query_top"));
+        product.sendKeys("Printed Chiffon Dress");
+        WebElement search=driver.findElement(By.name("submit_search"));
+        search.click();
+        Thread.sleep(10000);
+        List<WebElement> productCard = driver.findElement(By.className("product_list")).findElements(By.xpath("*"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(productCard.get(0)).click().build().perform();
+        Thread.sleep(15000);
+        WebElement color = driver.findElement(By.id("color_15"));
+        actions.moveToElement(color).click().build().perform();
+        WebElement addProduct = driver.findElement(By.name("Submit"));
+        addProduct.click();
+        WebElement dialog = driver.findElement(By.cssSelector("h2"));
+
+        Assert.assertEquals(dialog.getText(), "");
+
+
 
     }
 }
